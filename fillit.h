66c8b2dd88931f93b9a25a881b6421915593a850
Greg PHILIPS <gphilips@e1r3p5.42.fr>
2016-12-03 20:05:53 +0100

    ajout de proto

--- fillit.h
+++ fillit.h
@@ -1,13 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fillit.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gphilips <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/12/02 18:22:49 by gphilips          #+#    #+#             */
+/*   Updated: 2016/12/03 19:10:30 by gphilips         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef FILLIT_H
 # define FILLIT_H
 
 # include <unistd.h>
 # include <stdlib.h>
 # include <sys/types.h>
-# include <sys/stat.h>i
+# include <sys/stat.h>
 # include <fcntl.h>
+# include "libft/libft.h"
 
-#define BUFF_SIZE 20
+#define BUFF_SIZE 22
 
 typedef struct			s_tetris
 {
@@ -18,5 +31,9 @@ typedef struct			s_tetris
 	struct s_tetris		*next;
 }						t_tetris;
 
-char					ft_read_map(char *argv);
+int					ft_check_char(char *buffer);
+int					ft_check_line(char *buffer);
+int					ft_check_form(char *buffer);
 char					ft_nb_tetris(char *buff);
+
+#endif
