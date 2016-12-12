b5a9be015b44b05fcc3a53474cce0cc0b5174935
Greg Philips <gregphilips@MacBook-Pro-de-Greg.local>
2016-12-12 11:23:43 +0100

    OK

--- count_tetris.c
+++ count_tetris.c
@@ -1,6 +1,6 @@
 #include "fillit.h"
 
-char	ft_nb_tetris(char *buff)
+int		ft_count_tetris(char *buff)
 {
 	int		i;
 	int 	count;
@@ -9,7 +9,7 @@ char	ft_nb_tetris(char *buff)
 	count = 0;
 	while (buff[i])
 	{
-		if (buff[i] == '\n' && buff[i + 1] == '\n')
+		if (buff[i] == '\n' && (buff[i + 1] == '\n' || buff[i + 1] == '\0'))
 			count++;
 		i++;
 	}
